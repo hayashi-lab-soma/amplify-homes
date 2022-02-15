98940c1b7c14c5574fdb730f826d1b3708e01cfe
f-tominaga <tommy.tommy.walker@gmail.com>
2022-02-15 21:15:12 +0900

    pulled amplify repository,

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ import Amplify from 'aws-amplify';
 import "@aws-amplify/ui-react/styles.css";
 import {AmplifyProvider} from "@aws-amplify/ui-react";
 import awsconfig from './aws-exports';
+
+import {NewHomes, } from './ui-components'
+
 Amplify.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <AmplifyProvider>
       <App />
+      <NewHomes />
     </AmplifyProvider>
   </React.StrictMode>,
   document.getElementById('root')
